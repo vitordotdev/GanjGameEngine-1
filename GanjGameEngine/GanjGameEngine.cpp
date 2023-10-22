b3021f181eccfc5fd6b268f49e5e49402ee175de
Vitor.Dev <vitorfeliperamosmello8@gmail.com>
2023-10-22 12:47:13 -0300

    vs tools updated
    Visual Tools

---
+++ GanjGameEngine/GanjGameEngine.cpp
@@ -0,0 +1,10 @@
+// GanjGameEngine.cpp : Define as funções da biblioteca estática.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Este é um exemplo de uma função de biblioteca
+void fnGanjGameEngine()
+{
+}
